40394faec304b7d02cf7854271fc090e74539149
nypava <ny.dev0.em@gmail.com>
2026-03-19 10:48:18 +0300

    build: remove web build

--- build.zig
+++ build.zig
@@ -10,7 +10,6 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
     const raylib = raylib_dep.module("raylib");
-    const raylib_artifact = raylib_dep.artifact("raylib");
 
     const exe_mod = b.createModule(.{
         .root_source_file = b.path("src/main.zig"),
@@ -21,52 +20,18 @@ pub fn build(b: *std.Build) !void {
 
     const run_step = b.step("run", "Run the app");
 
-    //web exports are completely separate
-    if (target.query.os_tag == .emscripten) {
-        const emsdk = rlz.emsdk;
-        const wasm = b.addLibrary(.{
-            .name = "mirae",
-            .root_module = exe_mod,
-        });
+    const exe = b.addExecutable(.{
+        .name = "mirae",
+        .root_module = exe_mod,
+    });
+    b.installArtifact(exe);
 
-        const install_dir: std.Build.InstallDir = .{ .custom = "web" };
-        const emcc_flags = emsdk.emccDefaultFlags(b.allocator, .{ .optimize = optimize });
-        const emcc_settings = emsdk.emccDefaultSettings(b.allocator, .{ .optimize = optimize });
-
-        const emcc_step = emsdk.emccStep(b, raylib_artifact, wasm, .{
-            .optimize = optimize,
-            .flags = emcc_flags,
-            .settings = emcc_settings,
-            .shell_file_path = emsdk.shell(raylib_dep.builder),
-            .install_dir = install_dir,
-            .embed_paths = &.{.{ .src_path = "resources/" }},
-        });
-        b.getInstallStep().dependOn(emcc_step);
-
-        const html_filename = try std.fmt.allocPrint(b.allocator, "{s}.html", .{wasm.name});
-        const emrun_step = emsdk.emrunStep(
-            b,
-            b.getInstallPath(install_dir, html_filename),
-            &.{},
-        );
-
-        emrun_step.dependOn(emcc_step);
-        run_step.dependOn(emrun_step);
-    } else {
-        const exe = b.addExecutable(.{
-            .name = "mirae",
-            .root_module = exe_mod,
-        });
-        b.installArtifact(exe);
-
-        const run_cmd = b.addRunArtifact(exe);
+    const run_cmd = b.addRunArtifact(exe);
     
-        if (b.args) |args| {
-            run_cmd.addArgs(args);
-        }
-
-        run_cmd.step.dependOn(b.getInstallStep());
-
-        run_step.dependOn(&run_cmd.step);
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
     }
+
+    run_cmd.step.dependOn(b.getInstallStep());
+    run_step.dependOn(&run_cmd.step);
 }
